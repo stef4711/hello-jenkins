a14ad0c00f5f6e8d3aa0930be9c7015b24b8d580
stef4711 <stefan.bergstein@gmail.com>
2015-03-29 07:30:47 +0200

    Switch test to hello jenkins

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello world', done);
+    request(app).get('/').expect('hello jenkins', done);
   });
 });
 
